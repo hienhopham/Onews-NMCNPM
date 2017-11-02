aaeedbd3f0183388bbe15ab1ba5780dbbfa33cbb
dongsonbk58 <20133319@student.hust.edu.vn>
2017-11-03 02:33:43 +0700

    edit fragment main and icon in menu

---
+++ Onews-mobile-android/Onews/app/src/main/java/com/example/dongson/onews/Models/Tab.java
@@ -0,0 +1,31 @@
+package com.example.dongson.onews.Models;
+
+/**
+ * Created by Dong Son on 03-Nov-17.
+ */
+
+public class Tab {
+    private String tab_name;
+    private String tab_url;
+
+    public Tab(String tab_name, String tab_url) {
+        this.tab_name = tab_name;
+        this.tab_url = tab_url;
+    }
+
+    public String getTab_name() {
+        return tab_name;
+    }
+
+    public void setTab_name(String tab_name) {
+        this.tab_name = tab_name;
+    }
+
+    public String getTab_url() {
+        return tab_url;
+    }
+
+    public void setTab_url(String tab_url) {
+        this.tab_url = tab_url;
+    }
+}
