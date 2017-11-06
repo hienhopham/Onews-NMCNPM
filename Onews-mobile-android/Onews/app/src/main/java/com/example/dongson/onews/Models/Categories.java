bf895eb375db5e2730c65b9c7ebc9927d47081a1
TrangNguyen0110 <33047479+TrangNguyen0110@users.noreply.github.com>
2017-11-06 17:15:04 +0700

    add models categories and hot_topics

---
+++ Onews-mobile-android/Onews/app/src/main/java/com/example/dongson/onews/Models/Categories.java
@@ -0,0 +1,72 @@
+package com.example.dongson.onews.Models;
+
+/**
+ * Created by TrangRua on 11/6/2017.
+ */
+
+public class Categories {
+    private Integer id;
+    private String name;
+    private String level;
+    private Integer parents_id;
+    private String deleted;
+
+    public Categories(Integer id, String name, String level, Integer parents_id, String deleted) {
+        this.id = id;
+        this.name = name;
+        this.level = level;
+        this.parents_id = parents_id;
+        this.deleted = deleted;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public Integer getParents_id() {
+        return parents_id;
+    }
+
+    public void setParents_id(Integer parents_id) {
+        this.parents_id = parents_id;
+    }
+
+    public String getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(String deleted) {
+        this.deleted = deleted;
+    }
+
+    @Override
+    public String toString() {
+        return "Categories{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", level='" + level + '\'' +
+                ", parents_id=" + parents_id +
+                ", deleted='" + deleted + '\'' +
+                '}';
+    }
+}
