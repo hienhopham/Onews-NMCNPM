011f6fde4e26fc74fa30deb0ad67d89a37e37c9d
dongsonbk58 <20133319@student.hust.edu.vn>
2017-11-24 00:01:08 +0700

    add login with facebook

--- Onews-mobile-android/Onews/app/src/main/java/com/example/dongson/onews/view/LoginActivity.java
+++ Onews-mobile-android/Onews/app/src/main/java/com/example/dongson/onews/view/LoginActivity.java
@@ -1,39 +1,69 @@
 package com.example.dongson.onews.view;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.dongson.onews.Common.AlertDialogManager;
 import com.example.dongson.onews.Models.SessionManager;
 import com.example.dongson.onews.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import static java.security.AccessController.getContext;
 
 public class LoginActivity extends AppCompatActivity {
     private Button bt_register;
     private Button bt_login;
+    private Button bt_facebook;
     private EditText ed_username;
     private EditText ed_password;
     private SessionManager session;
     private AlertDialogManager alert = new AlertDialogManager();
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+        callbackManager = CallbackManager.Factory.create();
 
         session = new SessionManager(getApplicationContext());
         ed_username = (EditText) findViewById(R.id.ed_username_login);
         ed_password = (EditText) findViewById(R.id.ed_password_login);
         bt_login = (Button) findViewById(R.id.bt_login_login);
         bt_register = (Button) findViewById(R.id.bt_register_login);
+        bt_facebook = (Button) findViewById(R.id.bt_facebook_login);
         bt_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,6 +94,80 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                GraphRequest request = GraphRequest.newMeRequest(
+                        accessToken,
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                try {
+                                    String userName = (String) object.get("name");
+                                    Toast.makeText(LoginActivity.this, userName, Toast.LENGTH_LONG).show();
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,link,birthday,picture");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
+
+
+        PackageInfo info;
+        try {
+            info = getPackageManager().getPackageInfo("com.example.dongson.onews", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                //String something = new String(Base64.encodeBytes(md.digest()));
+                Log.e("hash key", something);
+            }
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e("name not found", e1.toString());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("no such an algorithm", e.toString());
+        } catch (Exception e) {
+            Log.e("exception", e.toString());
+        }
+
+    }
+
+    public void onFacebookLogInClicked( View view ){
+        LoginManager
+                .getInstance()
+                .logInWithReadPermissions(
+                        this,
+                        Arrays.asList("public_profile", "user_friends", "email")
+                );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
